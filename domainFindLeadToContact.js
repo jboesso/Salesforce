376580411a2737dcbed3701f3bf64572b7f89de6
João Boesso <jboesso@gmail.com>
2015-05-21 19:52:40 -0300

    Final domain lead e Trigger Criacao Opp
    Update final do .js de desqualificar lead e trigger de criacao de opp

--- domainFindLeadToContact.js
+++ domainFindLeadToContact.js
@@ -1,31 +1,103 @@
 {!REQUIRESCRIPT("/soap/ajax/33.0/connection.js")} 
 {!REQUIRESCRIPT("//code.jquery.com/jquery-2.1.4.min.js")}
-{!REQUIRESCRIPT("//raw.githubusercontent.com/jashkenas/underscore/master/underscore.js")}
 
 var recordsLeads = {!GETRECORDIDS($ObjectType.Lead)};
 var recordsContacts = {!GETRECORDIDS($ObjectType.Contact)}
 var newRecords = [];
+var uniqueDomains = [];
+var fullContacts = [];
+var cLeads = recordsLeads;
+var countSuccess = [];
+var count = 0;
 
 
 
+function finishHim() {
+	count++;
+	console.log(count, cLeads, "logando")
+	    // Da reload na pagina
+	if (count == cLeads) {
+	    alert("Foram encontrados "+countSuccess.length+" leads que já são clientes\ne "+(recordsLeads.length-countSuccess.length)+" são novos")
+	    result = sforce.connection.update(leads);
+	    console.log('count final',count, result);
+	    window.location.reload();
+	};
+}
 
 if (recordsLeads.length < 1) {
     alert("Por favor selecione algum lead");
-} else {
-    var r = confirm("Clique ''OK'' para para desqualificar os leads que já são clientes");
-	if (r) {
-		        var leads = sforce.connection.retrieve("Id,domain__c", "Lead", recordsLeads);
+} 	else {
+	    var r = confirm("Clique ''OK'' para para desqualificar os leads que já são clientes");
+		if (r) {
+		        var leads = sforce.connection.retrieve("Id,domain__c,Status,Motivo__c", "Lead", recordsLeads);
 		        var contacts = sforce.connection.query("select Id, domain__c from Contact where domain__c != null");
-		        console.log(contacts)
-		        console.log(contacts[2300])
-		        var uniqueDomains = [];
-		        //for ( var a = 0; )
-				//$.each(contacts, function(i, el){
-    			//if($.inArray(el, uniqueDomains) === -1) uniqueDomains.push(el);
-    			//console.log(uniqueDomains)
-//});
-	}
-};
+		        fullContacts = fullContacts.concat(contacts.records)
+		        console.log(contacts.records.length)
+		        //var contactsClientesInativos = sforce.connection.query("select Id, domain__c from Contact where domain__c != null AND Account.Type = 'Cliente Inativo'");
+		        var limit = 2000
+		        var pagination = Math.ceil(contacts.size / limit)
+		        for ( var cc = 1; cc < pagination;cc++) {
+		        	//var startLine = limit * cc
+		        	var contactsClientes = sforce.connection.query("select Id, domain__c from Contact where domain__c != null OFFSET " + limit)
+		        	fullContacts = fullContacts.concat(contactsClientes.records);
+		        	console.log(fullContacts.length)
+		        	
+		        }
+
+	    		for (var i=0;i<fullContacts.length;i++) {
+					if (uniqueDomains.indexOf(fullContacts[i].domain__c) == -1){
+		        	uniqueDomains.push(fullContacts[i].domain__c);		
+					} 
+				}
+
+				var cLeads = recordsLeads.length
+				for (var n = 0; n < cLeads; n++) {
+        	    	var domain = leads[n].domain__c;
+	            	console.log(leads[n].Id,"Salesforce",leads[n].domain__c, n)
+	            	if (leads[n].domain__c && leads[n].domain__c !== null) {
+		                // Le a pagina do usuário procurando pelo URL do facebook    
+				        if (uniqueDomains.indexOf(leads[n].domain__c) != -1 ) {
+							leads[n].Status = 'Desqualificado';
+							leads[n].Motivo__c = 'Cliente';
+							countSuccess.push('Cliente');
+							console.log("log indexof leads",leads[n].domain__c)
+							finishHim();
+						} else {
+		              	  finishHim();
+		            } 
+				   	} else {
+		                finishHim();
+        		}
+			}
+		}
+	}	
+
+               
+		        
+		        /*
+					for (var i=0;i<contactsClientes.records.length;i++) {
+						
+						if (uniqueDomainsClientes.indexOf(contactsClientes.records[i].domain__c) == -1){
+				        	uniqueDomainsClientes.push(contactsClientes.records[i].domain__c);		
+						}
+					}
+
+					var uniqueDomainsClientesInativos = [];
+		        
+					for (var i=0;i<contactsClientesInativos.records.length;i++) {
+						
+						if (uniqueDomainsClientesInativos.indexOf(contactsClientesInativos.records[i].domain__c) == -1){
+				        	uniqueDomainsClientesInativos.push(contactsClientesInativos.records[i].domain__c);		
+						}
+					}
+	console.log(contactsClientes.records.length)
+	console.log(uniqueDomainsClientes.length)
+	console.log(leads.domain__c)
+	console.log(contactsClientesInativos.records.length)
+	console.log(uniqueDomainsClientesInativos.length)
+*/
+//	}
+//};
 
 
 
